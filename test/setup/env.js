1dfd34cd139c9d12a3fbb583b0023463299b753c
Emil Ajdyna <emil@ajdyna.com>
2024-03-28 09:34:53 +0100

    Remove ava in favor of node:test and node:assert

--- test/setup/env.js
+++
@@ -1,2 +0,0 @@
-const env = require('browser-env')
-env()
--- test/unit.js
+++ test/unit.js
@@ -1,35 +1,39 @@
-const test = require('ava')
-const sinon = require('sinon')
-const Kinetic = require('../build')
+const env = require("browser-env")
+env()
 
-test('it has a lifecycle', assert => {
-  const node = document.createElement('div')
+const test = require("node:test")
+const assert = require("node:assert")
+const sinon = require("sinon")
+const Kinetic = require("../build")
+
+test("it has a lifecycle", () => {
+  const node = document.createElement("div")
   const kinetic = new Kinetic(node)
-  assert.truthy(kinetic)
+  assert(kinetic)
   kinetic.destroy()
 })
 
-test('it unbinds all document listeners', assert => {
-  const node = document.createElement('div')
-  sinon.spy(document.documentElement, 'addEventListener')
-  sinon.spy(document.documentElement, 'removeEventListener')
+test("it unbinds all document listeners", () => {
+  const node = document.createElement("div")
+  sinon.spy(document.documentElement, "addEventListener")
+  sinon.spy(document.documentElement, "removeEventListener")
   const kinetic = new Kinetic(node)
   kinetic.destroy()
   const actual = document.documentElement.addEventListener.callCount
   const expected = document.documentElement.removeEventListener.callCount
-  assert.truthy(actual > 0)
+  assert(actual > 0)
   assert.deepEqual(actual, expected)
 })
 
-test('it unbinds all node listeners', assert => {
-  const node = document.createElement('div')
-  sinon.spy(node, 'addEventListener')
-  sinon.spy(node, 'removeEventListener')
+test("it unbinds all node listeners", () => {
+  const node = document.createElement("div")
+  sinon.spy(node, "addEventListener")
+  sinon.spy(node, "removeEventListener")
   const kinetic = new Kinetic(node)
   kinetic.destroy()
   const actual = node.addEventListener.callCount
   const expected = node.removeEventListener.callCount
-  assert.truthy(actual > 0)
+  assert(actual > 0)
   assert.deepEqual(actual, expected)
   node.addEventListener.restore()
   node.removeEventListener.restore()
