b655987cda918b4e5520bd864592341983e1eb29
Emil Ajdyna <emil@ajdyna.com>
2024-03-28 10:04:05 +0100

    Update @rollup/plugin-terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,32 +1,30 @@
-import babel from '@rollup/plugin-babel'
-import { terser } from 'rollup-plugin-terser'
+import babel from "@rollup/plugin-babel"
+import terser from "@rollup/plugin-terser"
 
 export default {
-  input: 'index.js',
+  input: "index.js",
   output: {
-    file: 'build/index.js',
-    format: 'umd',
-    name: 'Kinetic'
+    file: "build/index.js",
+    format: "umd",
+    name: "Kinetic",
   },
   plugins: [
     babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
       presets: [
         [
-          '@babel/preset-env',
+          "@babel/preset-env",
           {
             targets: {
-              browsers: ['last 2 versions', 'ie >= 8']
+              browsers: ["last 2 versions", "ie >= 8"],
             },
-            modules: false
-          }
-        ]
+            modules: false,
+          },
+        ],
       ],
-      plugins: [
-        '@babel/plugin-transform-object-assign'
-      ]
+      plugins: ["@babel/plugin-transform-object-assign"],
     }),
-    terser()
-  ]
+    terser(),
+  ],
 }
